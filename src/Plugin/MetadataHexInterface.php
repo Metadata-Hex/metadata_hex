20a954502ac42d90b93636939471cf7660cd5e21
David Belich <david@codekoalas.com>
2025-02-04 19:53:49 -0600

    trying some new stuff

---
+++ src/Plugin/MetadataHexInterface.php
@@ -0,0 +1,8 @@
+namespace Drupal\metadata_hex\Plugin;
+
+use Drupal\Component\Plugin\PluginInspectionInterface;
+
+interface MetadataHexInterface extends PluginInspectionInterface {
+  public function process();
+}
+
