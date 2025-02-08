4698f1ff326984224578cd39276b399ebe39f6d0
davidbelich <43216841+davidbelich@users.noreply.github.com>
2025-02-07 21:41:51 -0600

    Reverting form layout

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -41,7 +41,7 @@ class SettingsForm extends ConfigFormBase {
     $config = $this->config('metadata_hex.settings');
 
     $form['extraction_settings'] = [
-      '#type' => 'details',
+      '#type' => 'vertical_tabs',
       '#title' => $this->t('Extraction Settings'),
       '#open' => true,
     ];
@@ -54,27 +54,29 @@ class SettingsForm extends ConfigFormBase {
     ];
 
     $form['extraction_settings']['field_mappings'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Field mappings (PDF to Drupal)'),
+      '#type' => 'textarea',
+      '#title' => $this->t('Field mappings (File to Drupal)'),
+      '#default_value' => $config->get('field_mappings'),
+      '#description' => $this->t('Enter each field mappings in the format "file_field | drupal_field" on a new line.'),
     ];
 
-    $field_mappings = $config->get('extraction_settings.field_mappings') ?? [];
-    foreach ($field_mappings as $index => $mapping) {
-      $form['extraction_settings']['field_mappings'][$index] = [
-        '#type' => 'fieldset',
-        '#title' => $this->t('Mapping @index', ['@index' => $index + 1]),
-      ];
-      $form['extraction_settings']['field_mappings'][$index]['pdf_field'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('PDF Field'),
-        '#default_value' => $mapping['pdf_field'] ?? '',
-      ];
-      $form['extraction_settings']['field_mappings'][$index]['drupal_field'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('Drupal Field'),
-        '#default_value' => $mapping['drupal_field'] ?? '',
-      ];
-    }
+    // $field_mappings = $config->get('extraction_settings.field_mappings') ?? [];
+    // foreach ($field_mappings as $index => $mapping) {
+    //   $form['extraction_settings']['field_mappings'][$index] = [
+    //     '#type' => 'fieldset',
+    //     '#title' => $this->t('Mapping @index', ['@index' => $index + 1]),
+    //   ];
+    //   $form['extraction_settings']['field_mappings'][$index]['pdf_field'] = [
+    //     '#type' => 'textfield',
+    //     '#title' => $this->t('PDF Field'),
+    //     '#default_value' => $mapping['pdf_field'] ?? '',
+    //   ];
+    //   $form['extraction_settings']['field_mappings'][$index]['drupal_field'] = [
+    //     '#type' => 'textfield',
+    //     '#title' => $this->t('Drupal Field'),
+    //     '#default_value' => $mapping['drupal_field'] ?? '',
+    //   ];
+    // }
 
     $form['extraction_settings']['strict_handling'] = [
       '#type' => 'checkbox',
@@ -107,17 +109,31 @@ class SettingsForm extends ConfigFormBase {
       '#open' => true,
     ];
 
+    $content_types = NodeType::loadMultiple();
+    $options = [];
+    foreach ($content_types as $content_type) {
+      $options[$content_type->id()] = $content_type->label();
+    }
+
     $form['node_process']['bundle_types'] = [
-      '#type' => 'textarea',
+      '#type' => 'select',
       '#title' => $this->t('Bundle types to process'),
+      '#multiple' => TRUE,
+      '#options' => $options,
       '#default_value' => implode("\n", $config->get('node_process.bundle_types') ?? []),
-      '#description' => $this->t('Enter bundle types, one per line.'),
+      '#description' => $this->t('Select multiple node bundle types to indicate which types will be preprocessed for PDF metadata extraction.'),
     ];
 
     $form['node_process']['allow_reprocess'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Allow reprocessing of already processed nodes'),
       '#default_value' => $config->get('node_process.allow_reprocess'),
+    ];
+
+    $form['node_process']['process_nodes'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Manually process all selected node types'),
+      '#submit' => ['::processAllNodes'],
     ];
 
     $form['file_ingest'] = [
@@ -145,7 +161,29 @@ class SettingsForm extends ConfigFormBase {
       '#description' => $this->t('Enter the directory path where files should be ingested.'),
     ];
 
+    $form['file_ingest']['process_cron_nodes'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Manually trigger file ingest'),
+      '#submit' => ['::processAllPdfs'],
+    ];
+
+
     return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
+
+    $fieldMappings = $form_state->getValue('field_mappings');
+    $validator = new FormValidator($this->mapping);
+    $result = $validator->validateForm();
+
+    if ($result !== true) {
+      $form_state->setErrorByName('field_mappings', $result);
+    }
   }
 
   /**
@@ -159,24 +197,15 @@ class SettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('metadata_hex.settings');
 
-    $config->set('extraction_settings.hook_node_types', array_filter(array_map('trim', explode("\n", $form_state->getValue('hook_node_types')))));
+    $config->set('extraction_settings.hook_node_types', $form_state->getValue('hook_node_types'));
     
-    $config->set('extraction_settings.field_mappings', []);
-    foreach ($form_state->getValue('field_mappings') as $mapping) {
-      if (!empty($mapping['pdf_field']) && !empty($mapping['drupal_field'])) {
-        $config->append('extraction_settings.field_mappings', [
-          'pdf_field' => $mapping['pdf_field'],
-          'drupal_field' => $mapping['drupal_field'],
-        ]);
-      }
-    }
-
+    $config->set('extraction_settings.field_mappings', $form_state->getValue('field_mappings'));
     $config->set('extraction_settings.strict_handling', $form_state->getValue('strict_handling'));
     $config->set('extraction_settings.data_protected', $form_state->getValue('data_protected'));
     $config->set('extraction_settings.title_protected', $form_state->getValue('title_protected'));
-    $config->set('extraction_settings.available_extensions', array_filter(array_map('trim', explode("\n", $form_state->getValue('available_extensions')))));
+    $config->set('extraction_settings.available_extensions', $form_state->getValue('available_extensions'));
 
-    $config->set('node_process.bundle_types', array_filter(array_map('trim', explode("\n", $form_state->getValue('bundle_types')))));
+    $config->set('node_process.bundle_types', $form_state->getValue('bundle_types'));
     $config->set('node_process.allow_reprocess', $form_state->getValue('allow_reprocess'));
 
     $config->set('file_ingest.bundle_type_for_generation', $form_state->getValue('bundle_type_for_generation'));
