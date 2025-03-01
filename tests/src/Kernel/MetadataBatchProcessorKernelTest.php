6125e414932532501e755b1d01f4171fbd3d6404
David Belich <43216841+davidbelich@users.noreply.github.com>
2025-03-01 13:09:42 -0600

    Initial test cases split out

--- tests/src/Kernel/MetadataBatchProcessorKernelTest.php
+++ tests/src/Kernel/MetadataBatchProcessorKernelTest.php
@@ -15,7 +15,7 @@ class MetadataBatchProcessorKernelTest extends BaseKernelTestHex {
    * Tests processing a node with a valid PDF file.
    */
   public function testProcessNodeWithValidFileTypeNoMetadata() {
-    // $this->expectException(\Drupal\Core\Entity\EntityStorageException::class);
+
     $this->expectException(\Exception::class);
     $this->expectExceptionMessage("Invalid or unreadable file: vfs://root/test_document.pdf");
 
@@ -121,4 +121,327 @@ class MetadataBatchProcessorKernelTest extends BaseKernelTestHex {
     $this->assertContains('Metadata', $term_names, "The expected taxonomy term name Metadata is not present.");
 
   }
+
+ 
+  /**
+   * 
+   
+
+    - remove article, dooesnt run/extract 
+    - change field_mappings, doesnt extract changed
+    - flatten keys = false
+    - strict_handling = true
+    - data_protected = true
+    - title_protechted = false
+   * 
+   * 
+   */
+
+   /**
+   * Tests processing .
+   */
+  public function testProcessNodeWithTitleProtected() {
+    // Setup an actual valid pdf file with metadata and node
+    // CHANGE
+    $this->setConfigSetting('extraction_settings.title_protected', FALSE);
+
+    $file = $this->createDrupalFile('test_metadata.pdf', $this->generatePdfWithMetadata(), 'application/pdf');
+    $node = $this->createNode($file);
+
+    // Capture the original details
+    $created = $node->getCreatedTime();
+    $modified = $node->getChangedTime();
+
+    // Process the node
+    $this->batchProcessor->processNode($node->id());
+
+    // Reload the node now that batch processes have occured
+    $node_alt = \Drupal::entityTypeManager()->getStorage('node')->load($node->id());
+
+    // Capture the current details
+    $created_alt = $node_alt->getCreatedTime();
+    $modified_alt = $node_alt->getChangedTime();
+    $fsubj = $node_alt->get('field_subject')->getString();
+    $fpages = $node_alt->get('field_pages')->getString();
+    $fdate = $node_alt->get('field_publication_date')->getString();
+    $ftype = $node_alt->get('field_file_type')->value;
+    $ftop = $node_alt->get('field_topics')->getValue();
+    $term_names = [];
+    foreach ($node_alt->get('field_topics')->referencedEntities() as $term) {
+        $term_names[] = $term->label();
+    }
+
+    // ASSERTATIONS
+    $this->assertEquals($created, $created_alt, 'Node creation dates dont match');
+
+    $this->assertNotEquals('', $fsubj, 'Subject is blank');
+    $this->assertEquals('Testing Metadata in PDFs', $fsubj, 'Extracted subject doesnt match expected');
+
+    $this->assertNotEquals('', $fpages, 'Catalog is blank');
+    $this->assertEquals(1, $fpages, 'Extracted catalog doesnt match expected');
+
+    $this->assertNotEquals('', $fdate, 'Publication date is blank');
+    $this->assertNotFalse(strtotime($fdate), "The publication date is not a valid date timestamp.");
+
+    $this->assertNotEquals('', $ftype, 'FileType is blank');
+    $this->assertEquals('pdf', $ftype, 'Extracted file_type doesnt match expected');
+
+    $this->assertNotEquals('', $ftop, 'Topic is blank');
+    $this->assertContains('Drupal', $term_names, "The expected taxonomy term name Drupal is not present.");
+    $this->assertContains('TCPDF', $term_names, "The expected taxonomy term name TCPDF is not present.");
+    $this->assertContains('Test', $term_names, "The expected taxonomy term name Test is not present.");
+    $this->assertContains('Metadata', $term_names, "The expected taxonomy term name Metadata is not present.");
+    }
+
+   /**
+   * Tests processing .
+   */
+  public function testProcessNodeWithDataProtected() {
+    // Setup an actual valid pdf file with metadata and node
+    // CHANGE
+    $this->setConfigSetting('extraction_settings.data_protected', TRUE);
+
+    $file = $this->createDrupalFile('test_metadata.pdf', $this->generatePdfWithMetadata(), 'application/pdf');
+    $node = $this->createNode($file);
+
+    // Capture the original details
+    $created = $node->getCreatedTime();
+    $modified = $node->getChangedTime();
+
+    // Process the node
+    $this->batchProcessor->processNode($node->id());
+
+    // Reload the node now that batch processes have occured
+    $node_alt = \Drupal::entityTypeManager()->getStorage('node')->load($node->id());
+
+    // Capture the current details
+    $created_alt = $node_alt->getCreatedTime();
+    $modified_alt = $node_alt->getChangedTime();
+    $fsubj = $node_alt->get('field_subject')->getString();
+    $fpages = $node_alt->get('field_pages')->getString();
+    $fdate = $node_alt->get('field_publication_date')->getString();
+    $ftype = $node_alt->get('field_file_type')->value;
+    $ftop = $node_alt->get('field_topics')->getValue();
+    $term_names = [];
+    foreach ($node_alt->get('field_topics')->referencedEntities() as $term) {
+        $term_names[] = $term->label();
+    }
+
+    // ASSERTATIONS
+    $this->assertEquals($created, $created_alt, 'Node creation dates dont match');
+
+    $this->assertNotEquals('', $fsubj, 'Subject is blank');
+    $this->assertEquals('Testing Metadata in PDFs', $fsubj, 'Extracted subject doesnt match expected');
+
+    $this->assertNotEquals('', $fpages, 'Catalog is blank');
+    $this->assertEquals(1, $fpages, 'Extracted catalog doesnt match expected');
+
+    $this->assertNotEquals('', $fdate, 'Publication date is blank');
+    $this->assertNotFalse(strtotime($fdate), "The publication date is not a valid date timestamp.");
+
+    $this->assertNotEquals('', $ftype, 'FileType is blank');
+    $this->assertEquals('pdf', $ftype, 'Extracted file_type doesnt match expected');
+
+    $this->assertNotEquals('', $ftop, 'Topic is blank');
+    $this->assertContains('Drupal', $term_names, "The expected taxonomy term name Drupal is not present.");
+    $this->assertContains('TCPDF', $term_names, "The expected taxonomy term name TCPDF is not present.");
+    $this->assertContains('Test', $term_names, "The expected taxonomy term name Test is not present.");
+    $this->assertContains('Metadata', $term_names, "The expected taxonomy term name Metadata is not present.");
+    }
+
+/**
+   * Tests processing .
+   */
+  public function testProcessNodeWithStrictHandling() {
+    // Setup an actual valid pdf file with metadata and node
+    // CHANGE
+    $this->setConfigSetting('extraction_settings.strict_handling', TRUE);
+
+    $file = $this->createDrupalFile('test_metadata.pdf', $this->generatePdfWithMetadata(), 'application/pdf');
+    $node = $this->createNode($file);
+
+    // Capture the original details
+    $created = $node->getCreatedTime();
+    $modified = $node->getChangedTime();
+
+    // Process the node
+    $this->batchProcessor->processNode($node->id());
+
+    // Reload the node now that batch processes have occured
+    $node_alt = \Drupal::entityTypeManager()->getStorage('node')->load($node->id());
+
+    // Capture the current details
+    $created_alt = $node_alt->getCreatedTime();
+    $modified_alt = $node_alt->getChangedTime();
+    $fsubj = $node_alt->get('field_subject')->getString();
+    $fpages = $node_alt->get('field_pages')->getString();
+    $fdate = $node_alt->get('field_publication_date')->getString();
+    $ftype = $node_alt->get('field_file_type')->value;
+    $ftop = $node_alt->get('field_topics')->getValue();
+    $term_names = [];
+    foreach ($node_alt->get('field_topics')->referencedEntities() as $term) {
+        $term_names[] = $term->label();
+    }
+
+    // ASSERTATIONS
+    $this->assertEquals($created, $created_alt, 'Node creation dates dont match');
+
+    $this->assertNotEquals('', $fsubj, 'Subject is blank');
+    $this->assertEquals('Testing Metadata in PDFs', $fsubj, 'Extracted subject doesnt match expected');
+
+    $this->assertNotEquals('', $fpages, 'Catalog is blank');
+    $this->assertEquals(1, $fpages, 'Extracted catalog doesnt match expected');
+
+    $this->assertNotEquals('', $fdate, 'Publication date is blank');
+    $this->assertNotFalse(strtotime($fdate), "The publication date is not a valid date timestamp.");
+
+    $this->assertNotEquals('', $ftype, 'FileType is blank');
+    $this->assertEquals('pdf', $ftype, 'Extracted file_type doesnt match expected');
+
+    $this->assertNotEquals('', $ftop, 'Topic is blank');
+    $this->assertContains('Drupal', $term_names, "The expected taxonomy term name Drupal is not present.");
+    $this->assertContains('TCPDF', $term_names, "The expected taxonomy term name TCPDF is not present.");
+    $this->assertContains('Test', $term_names, "The expected taxonomy term name Test is not present.");
+    $this->assertContains('Metadata', $term_names, "The expected taxonomy term name Metadata is not present.");
+    }
+
+/**
+   * Tests processing .
+   */
+  public function testProcessNodeWithFlattenKeys() {
+    // Setup an actual valid pdf file with metadata and node
+    // CHANGE
+    $this->setConfigSetting('extraction_settings.flatten_keys', FALSE);
+
+    $file = $this->createDrupalFile('test_metadata.pdf', $this->generatePdfWithMetadata(), 'application/pdf');
+    $node = $this->createNode($file);
+
+    // Capture the original details
+    $created = $node->getCreatedTime();
+    $modified = $node->getChangedTime();
+
+    // Process the node
+    $this->batchProcessor->processNode($node->id());
+
+    // Reload the node now that batch processes have occured
+    $node_alt = \Drupal::entityTypeManager()->getStorage('node')->load($node->id());
+
+    // Capture the current details
+    $created_alt = $node_alt->getCreatedTime();
+    $modified_alt = $node_alt->getChangedTime();
+    $fsubj = $node_alt->get('field_subject')->getString();
+    $fpages = $node_alt->get('field_pages')->getString();
+    $fdate = $node_alt->get('field_publication_date')->getString();
+    $ftype = $node_alt->get('field_file_type')->value;
+    $ftop = $node_alt->get('field_topics')->getValue();
+    $term_names = [];
+    foreach ($node_alt->get('field_topics')->referencedEntities() as $term) {
+        $term_names[] = $term->label();
+    }
+
+    // ASSERTATIONS
+    $this->assertEquals($created, $created_alt, 'Node creation dates dont match');
+
+    $this->assertNotEquals('', $fsubj, 'Subject is blank');
+    $this->assertEquals('Testing Metadata in PDFs', $fsubj, 'Extracted subject doesnt match expected');
+
+    $this->assertNotEquals('', $fpages, 'Catalog is blank');
+    $this->assertEquals(1, $fpages, 'Extracted catalog doesnt match expected');
+
+    $this->assertNotEquals('', $fdate, 'Publication date is blank');
+    $this->assertNotFalse(strtotime($fdate), "The publication date is not a valid date timestamp.");
+
+    $this->assertNotEquals('', $ftype, 'FileType is blank');
+    $this->assertEquals('pdf', $ftype, 'Extracted file_type doesnt match expected');
+
+    $this->assertNotEquals('', $ftop, 'Topic is blank');
+    $this->assertContains('Drupal', $term_names, "The expected taxonomy term name Drupal is not present.");
+    $this->assertContains('TCPDF', $term_names, "The expected taxonomy term name TCPDF is not present.");
+    $this->assertContains('Test', $term_names, "The expected taxonomy term name Test is not present.");
+    $this->assertContains('Metadata', $term_names, "The expected taxonomy term name Metadata is not present.");
+    }
+
+  /**
+   * Tests processing .
+   */
+  public function testProcessNodeWithBundleNotSelected() {
+  // Setup an actual valid pdf file with metadata and node
+  $this->setConfigSetting('extraction_settings.hook_node_types', ['page']);
+    $file = $this->createDrupalFile('test_metadata.pdf', $this->generatePdfWithMetadata(), 'application/pdf');
+    $node = $this->createNode($file);
+
+    $settingsManager = new \Drupal\metadata_hex\Service\SettingsManager();
+
+    // Capture the original details
+    $created = $node->getCreatedTime();
+    $modified = $node->getChangedTime();
+
+    // Process the node
+    $this->batchProcessor->processNode($node->id());
+
+    // Reload the node now that batch processes have occured
+    $node_alt = \Drupal::entityTypeManager()->getStorage('node')->load($node->id());
+
+    // ASSERTATIONS
+    $this->assertEquals($created, $created_alt, 'Node creation dates dont match');
+  }
+}
+
+
+/**
+   * Tests processing .
+   */
+  public function testProcessNodeWithFieldMapping() {
+    // Setup an actual valid pdf file with metadata and node
+    // CHANGE
+    $this->setConfigSetting('extraction_settings.field_mappings', "keywords|field_topics\ntitle|title\nsubject|field_subject\nCreationDate|field_pub_date\nPages|field_pages\nDC:Format|field_file_type");
+
+    $file = $this->createDrupalFile('test_metadata.pdf', $this->generatePdfWithMetadata(), 'application/pdf');
+    $node = $this->createNode($file);
+
+    // Capture the original details
+    $created = $node->getCreatedTime();
+    $modified = $node->getChangedTime();
+
+    // Process the node
+    $this->batchProcessor->processNode($node->id());
+
+    // Reload the node now that batch processes have occured
+    $node_alt = \Drupal::entityTypeManager()->getStorage('node')->load($node->id());
+
+    // Capture the current details
+    $created_alt = $node_alt->getCreatedTime();
+    $modified_alt = $node_alt->getChangedTime();
+    $fsubj = $node_alt->get('field_subject')->getString();
+    $fpages = $node_alt->get('field_pages')->getString();
+    $fdate = $node_alt->get('field_publication_date')->getString();
+    $ftype = $node_alt->get('field_file_type')->value;
+    $ftop = $node_alt->get('field_topics')->getValue();
+    $term_names = [];
+    foreach ($node_alt->get('field_topics')->referencedEntities() as $term) {
+        $term_names[] = $term->label();
+    }
+
+    // ASSERTATIONS
+    $this->assertEquals($created, $created_alt, 'Node creation dates dont match');
+
+    $this->assertNotEquals('', $fsubj, 'Subject is blank');
+    $this->assertEquals('Testing Metadata in PDFs', $fsubj, 'Extracted subject doesnt match expected');
+
+    $this->assertNotEquals('', $fpages, 'Catalog is blank');
+    $this->assertEquals(1, $fpages, 'Extracted catalog doesnt match expected');
+
+    $this->assertNotEquals('', $fdate, 'Publication date is blank');
+    $this->assertNotFalse(strtotime($fdate), "The publication date is not a valid date timestamp.");
+
+    $this->assertNotEquals('', $ftype, 'FileType is blank');
+    $this->assertEquals('pdf', $ftype, 'Extracted file_type doesnt match expected');
+
+    $this->assertNotEquals('', $ftop, 'Topic is blank');
+    $this->assertContains('Drupal', $term_names, "The expected taxonomy term name Drupal is not present.");
+    $this->assertContains('TCPDF', $term_names, "The expected taxonomy term name TCPDF is not present.");
+    $this->assertContains('Test', $term_names, "The expected taxonomy term name Test is not present.");
+    $this->assertContains('Metadata', $term_names, "The expected taxonomy term name Metadata is not present.");
+    }
+
 }
