c84b052004b27092546f063d55357f89bb42d4ec
David Belich <43216841+davidbelich@users.noreply.github.com>
2025-03-03 15:31:26 -0600

    qwe

--- tests/src/Kernel/BaseKernelTestHex.php
+++ tests/src/Kernel/BaseKernelTestHex.php
@@ -197,7 +197,7 @@ protected $settingsManager;
     $settings = [
         'extraction_settings.hook_node_types' => ['article', 'page'],
         'extraction_settings.field_mappings' => "keywords|field_topics\ntitle|title\nsubject|field_subject\nCreationDate|field_publication_date\nPages|field_pages\nDC:Format|field_file_type",
-        'extraction_settings.flatten_keys' => TRUE,
+        'extraction_settings.flatten_keys' => FALSE,
         'extraction_settings.strict_handling' => FALSE,
         'extraction_settings.data_protected' => FALSE,
         'extraction_settings.title_protected' => TRUE,
@@ -218,7 +218,7 @@ protected $settingsManager;
     $this->settingsManager = new \Drupal\metadata_hex\Service\SettingsManager();
 
     $this->assertEquals(false, $this->settingsManager->getStrictHandling(), 'Strict handling should be false');
-    $this->assertEquals(true, $this->settingsManager->getFlattenKeys(), 'flatten keys should be true');
+    $this->assertEquals(FALSE, $this->settingsManager->getFlattenKeys(), 'flatten keys should be true');
     $this->assertEquals(false, $this->settingsManager->getProtectedData(), 'data protection should be false');
     $this->assertEquals(true, $this->settingsManager->getProtectedTitle(), 'title protection should be true');
   }
